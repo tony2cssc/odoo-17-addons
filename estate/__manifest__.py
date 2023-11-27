9fee759bf9345743356480b79f3a84d67fecf184
Tony Guo <jijing.guo@outlook.com>
2023-11-28 00:20:29 +0800

    Initalize the configuration

---
+++ estate/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': "Real Estate",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Tony Guo",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+}
